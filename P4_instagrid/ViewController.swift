fb8355348bb03c8b84dee84b5f105fad208c4307
Elora <edelpierre@exomind.fr>
2022-05-02 10:26:59 +0200

    First commit

---
+++ P4_instagrid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P4_instagrid
+//
+//  Created by Elora on 29/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var layout1: UIImageView!
+    @IBOutlet weak var layout2: UIImageView!
+    @IBOutlet weak var layout3: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
