fa7dc05032105b33296b77533b0318e1a75db922
Elora <edelpierre@exomind.fr>
2022-05-05 09:59:57 +0200

    image picker controller done

--- P4_instagrid/ViewController.swift
+++ P4_instagrid/ViewController.swift
@@ -5,21 +5,76 @@
 //  Created by Elora on 29/04/2022.
 //
 
+//
+import SwiftUI
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var layout1: UIImageView!
-    @IBOutlet weak var layout2: UIImageView!
-    @IBOutlet weak var layout3: UIImageView!
     
+    // global var stocking the image view tapped
+    weak var imageView: UIImageView?
     
+    // function with in parameters the TGR activated
+    @IBAction func didTapImage(_ sender: UITapGestureRecognizer){
+        // transform the sender view in an image view
+        guard let senderImageView = sender.view as? UIImageView else {
+            return
+        }
+        // update de global var with the image view just tapped
+        imageView = senderImageView
+        // picker controller process
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+        
+    }
+    
+    // func with in parametres the TGR activated
+    @IBAction func changeLayout(_ sender: UITapGestureRecognizer){
+        // switch on the tag depending on the sender in parameter
+        switch sender.view?.tag {
+            
+        case 1:
+            // hidden le top left
+            print("change for Layout 1")
+        case 2:
+            //hidden le bottom left
+            print("change for layout 2")
+        case 3:
+            // aucun hidden
+            print("change for layout 3")
+        default:
+            print("default")
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+}
 
+// extension for the pickercontroller
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // convert in an image and launch it in the current imageView global var
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView?.image = image
+        }
+        // close the window when finish
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    // close the window with cancel button
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
